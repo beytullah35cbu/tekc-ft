c7b6a4189baf284ebcdb5dd4927c5fe45a5c7d50
beytullah <114831003+beytullah35cbu@users.noreply.github.com>
2023-10-30 14:25:59 +0300

    tekcıft

---
+++ tekcıft/tekcıft/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bir sayı girin: ");
+        int sayi = int.Parse(Console.ReadLine());
+
+        if (sayi % 2 == 0)
+        {
+            Console.WriteLine("Girilen sayı çifttir.");
+        }
+        else
+        {
+            Console.WriteLine("Girilen sayı tektir.");
+        }
+
+        Console.ReadKey();
+    }
+}
